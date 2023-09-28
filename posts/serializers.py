1eb7146e30b6e925429e8af7b11022083710d84b
KJH <kjh@KJHs-MacBook-Pro.local>
2023-09-28 22:36:58 +0900

    ✨ feat: 댓글 관련 기능 추가 - 별도 앱 아닌 Post 내 모델 생성

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
 from users.serializers import ProfileSerializer
-from .models import Post
+from .models import Post, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('pk', 'profile', 'post', 'text')
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('post', 'text')
+
 
 class PostSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only=True) # nested serializer: 시리얼라이져 내 또 다른 시리얼라이져를 넣어 이중연결하는 구조
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ('pk', 'profile', 'title', 'body', 'image', 'published_date', 'likes')
+        fields = ('pk', 'profile', 'title', 'body', 'image', 'published_date', 'likes', 'comments')
 
     
 class PostCreateSerializer(serializers.ModelSerializer):
